e9e9ab5559b85767e37a6a9f200c31e2543a5c13
kevinzhu12 <kbzhu@mit.edu>
2024-12-23 07:03:04 +0100

    fixed bug

--- app/projects/page.js and app/past-work/page.js
+++
@@ -1,9 +0,0 @@
-import MagicCardWrapper from "@/components/magic-card-wrapper";
-
-// Replace MagicCard with MagicCardWrapper
-<MagicCardWrapper
-  className="cursor-pointer flex-col shadow-sm bg-white hover:bg-cream-50 
-    dark:bg-dark-card dark:hover:bg-dark-hover transition-colors duration-200"
->
-  {/* ... card content ... */}
-</MagicCardWrapper>;
