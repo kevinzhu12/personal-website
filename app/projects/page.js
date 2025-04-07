1dcbe9f5591703ecdb43ff5160593c69bcb20c98
kevinzhu12 <kbzhu@mit.edu>
2025-04-07 12:06:42 -0400

    updated projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -56,7 +56,7 @@ export default function Projects() {
                 </h2>
               </a>
               <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
-                personal assistant on smart glasses (Mistral AI Prize)
+                personal assistant on smart glasses (Mistral AI Prize Winner)
               </h3>
               <p className="text-sm text-[#1A1A1A] dark:text-white mt-1">
                 Built a Mistral-powered personal assistant on AugmentOS and Even
@@ -76,68 +76,12 @@ export default function Projects() {
           <div className="flex">
             <div className="pr-2 py-3">
               <a
-                href="https://perplexity-clone-kappa.vercel.app/"
-                target="_blank"
-                className="hover:opacity-80"
-              >
-                <h2 className="text-xl font-bold text-[#1A1A1A] dark:text-white">
-                  PerplexiClone
-                </h2>
-              </a>
-              <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
-                AI-powered search engine
-              </h3>
-              <p className="text-sm text-[#1A1A1A] dark:text-white mt-1">
-                Built a Perplexity clone using Exa and OpenAI.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex">
-            <div className="pr-2 py-3">
-              <a
-                href="https://home.withmantis.com/"
-                target="_blank"
-                className="hover:opacity-80"
-              >
-                <h2 className="text-xl font-bold text-[#1A1A1A] dark:text-white">
-                  Mantis
-                </h2>
-              </a>
-              <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
-                Working on Mantis with{" "}
-                <a
-                  href="https://compbio.mit.edu/index.html"
-                  target="_blank"
-                  className="text-blue-500 hover:underline"
-                >
-                  Kellis Lab
-                </a>{" "}
-                at{" "}
-                <a
-                  href="https://www.csail.mit.edu/"
-                  target="_blank"
-                  className="text-blue-500 hover:underline"
-                >
-                  MIT CSAIL
-                </a>
-              </h3>
-              <p className="text-sm text-[#1A1A1A] dark:text-white mt-1">
-                Data visualization and cognitive cartography tool using LLMs to
-                visualize and interact with textual data.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex">
-            <div className="pr-2 py-3">
-              <a
                 href="https://devpost.com/software/jabber-ai"
                 target="_blank"
                 className="hover:opacity-80"
               >
                 <h2 className="text-xl font-bold text-[#1A1A1A] dark:text-white">
-                  Jabber AI
+                  Jabber
                 </h2>
               </a>
               <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
@@ -147,65 +91,6 @@ export default function Projects() {
                 Used Hume AI's EVI to create voice interaction. GPT then
                 processes user transcripts into organized notecards on a
                 whiteboard space.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex">
-            <div className="pr-2 py-3">
-              <h2 className="text-xl font-bold text-[#1A1A1A] dark:text-white">
-                JourneyGenie
-              </h2>
-              <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
-                generate road trip itineraries and map directions
-              </h3>
-              <p className="text-sm text-[#1A1A1A] dark:text-white mt-1">
-                Used GPT to generate trip itineraries from user input. Displayed
-                itinerary on map with Google Maps API.
-                <span className="text-blue-500"> *in progress</span>
-              </p>
-            </div>
-          </div>
-
-          <div className="flex">
-            <div className="pr-2 py-3">
-              <a
-                href="https://github.com/kevinzhu12/quick-def"
-                target="_blank"
-                className="hover:opacity-80"
-              >
-                <h2 className="text-xl font-bold text-[#1A1A1A] dark:text-white">
-                  QuickDef
-                </h2>
-              </a>
-              <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
-                chrome extension to explain text using GPT
-              </h3>
-              <p className="text-sm text-[#1A1A1A] dark:text-white mt-1">
-                A chrome extension that generates real-time explanations with a
-                popup interface.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex">
-            <div className="pr-2 py-3">
-              <a
-                href="https://quote-search3.vercel.app/"
-                target="_blank"
-                className="hover:opacity-80"
-              >
-                <h2 className="text-xl font-bold text-[#1A1A1A] dark:text-white">
-                  Quote Search
-                </h2>
-              </a>
-              <h3 className="text-base italic text-[#1A1A1A] dark:text-white mt-1">
-                efficient search engine for precise quotes
-              </h3>
-              <p className="text-sm text-[#1A1A1A] dark:text-white mt-1">
-                Used Fuse.js fuzzy-search library for approximate keyword
-                matching. Built incremental streaming feature to update search
-                results with each keystroke.
               </p>
             </div>
           </div>
